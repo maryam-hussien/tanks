b028231b1f5eec22d7cf6ea1fff71366e13c9817
maryam-hussien <maryamhussien122@gmail.com>
2022-12-23 16:43:24 +0200

    bullet class

---
+++ src/src/Bullet.java
@@ -0,0 +1,23 @@
+public class Bullet {
+    long start, lifeTime;
+    Directions direction;
+    float x, y;
+    boolean fired;
+    float initX, initY;
+    Bullet bullet;
+
+    public Bullet(Directions direction, float x, float y, int lifetime){
+        this.direction = direction;
+        this.x = initX = x;
+        this.y = initY = y;
+        this.fired = true;
+        start = System.currentTimeMillis();
+        lifeTime = lifetime;
+
+        public void invalidate(){
+            fired = start+lifeTime > System.currentTimeMillis();
+            int dX = (int) ((initX - x) * (initX - x));
+            int dY = (int) ((initY - y) * (initY - y));
+            this.fired = dX + dY < 150;
+        }
+}
