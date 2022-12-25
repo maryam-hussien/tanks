a9902fd2d90a26bc1254f04c41b627386785217d
mahmoud magdy <mahmoud.magdy11198@gmail.com>
2022-12-25 20:25:38 +0200

    player class updated update collision method

---
+++ src/src/Bomb.java
@@ -0,0 +1,13 @@
+public class Bomb {
+
+    public int x;
+    public int y;
+    public int speed = 3;
+    public int damage;
+
+    public Bomb(int x, int y , int damage) {
+        this.x = x;
+        this.y = y;
+        this.damage = damage;
+    }
+}
